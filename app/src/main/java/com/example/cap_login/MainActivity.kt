16bb945456f07f58b67f95c7f2c07573dcccf99a
HyeopG <wndguqdlsla@naver.com>
2021-07-28 16:47:32 +0900

    menu

--- app/src/main/java/com/example/cap_login/MainActivity.kt
+++ app/src/main/java/com/example/cap_login/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.cap_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main)
         //setContentView(R.layout.signup)
+
+        imageButton.setOnClickListener{ layout_drawer.openDrawer(GravityCompat.START) }
+        naviView.setNavigationItemSelectedListener(this)
     }
-}//수정테스트좀 하겠습니다.
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.info -> println("내 정보")
+            R.id.device -> println("디바이스")
+            R.id.deviceAdd -> println("장치 추가")
+        }
+        layout_drawer.closeDrawers()
+        return false
+    }
+
+    override fun onBackPressed() {
+        if (layout_drawer.isDrawerOpen(GravityCompat.START))
+        {
+            layout_drawer.closeDrawers()
+        }
+        else
+        {
+            super.onBackPressed()
+        }
+    }
+}
