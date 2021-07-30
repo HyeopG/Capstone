c73fd9f15279a76b909c349ca9c24fdae63f8336
HyeopG <wndguqdlsla@naver.com>
2021-07-30 10:49:04 +0900

    menu

--- app/src/main/java/com/example/cap_login/Main.kt
+++ app/src/main/java/com/example/cap_login/Main.kt
@@ -1,4 +1,57 @@
 package com.example.cap_login
 
-class Main {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.main.*
+
+
+class Main : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        //login창 - btnlogin버튼.
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        val btnSignup = findViewById<Button>(R.id.btn_signup)
+
+        btnLogin.setOnClickListener {
+            setContentView(R.layout.main)
+
+            imageButton.setOnClickListener{ layout_drawer.openDrawer(GravityCompat.START) }
+            naviView.setNavigationItemSelectedListener(this)
+        }
+        btnSignup.setOnClickListener{
+            setContentView(R.layout.signup)
+        }
+
+    }
+
+    // 네비게이터 구현.
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.info -> println("내 정보")
+            R.id.device -> println("디바이스")
+            R.id.deviceAdd -> println("장치 추가")
+        }
+        layout_drawer.closeDrawers()
+        return false
+    }
+
+    // 네비게이터에서 뒤로가기 키 눌렀을 때.
+    override fun onBackPressed() {
+        if (layout_drawer.isDrawerOpen(GravityCompat.START))
+        {
+            layout_drawer.closeDrawers()
+        }
+        else
+        {
+            super.onBackPressed()
+        }
+    }
 }
--- app/src/main/java/com/example/cap_login/MainActivity.kt
+++ app/src/main/java/com/example/cap_login/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.cap_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.MenuItem
-import android.view.View
 import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.main.*
@@ -16,10 +14,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.main)
         //setContentView(R.layout.signup)
 
+        // 네비게이터 버튼. R.layout.main에서만 사용가능.
         imageButton.setOnClickListener{ layout_drawer.openDrawer(GravityCompat.START) }
         naviView.setNavigationItemSelectedListener(this)
+
     }
 
+
+    // 네비게이터 구현.
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.info -> println("내 정보")
@@ -30,6 +32,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return false
     }
 
+    // 네비게이터에서 뒤로가기 키 눌렀을 때.
     override fun onBackPressed() {
         if (layout_drawer.isDrawerOpen(GravityCompat.START))
         {
